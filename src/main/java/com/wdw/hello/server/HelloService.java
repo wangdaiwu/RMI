b640b42e405acba8e4829850421193921d4be74e
wangdaiwu <62149679+wangdaiwu@users.noreply.github.com>
2022-04-21 19:18:31 +0800

    Delete src/main/java/com/wdw/hello directory

--- src/main/java/com/wdw/hello/server/HelloService.java
+++
@@ -1,8 +0,0 @@
-package com.wdw.hello.server;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface HelloService extends Remote {
-    public void sayHello() throws RemoteException;
-}
